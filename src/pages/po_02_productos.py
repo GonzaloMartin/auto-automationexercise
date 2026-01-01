d7d2cc29cdecd3ccb99e6e804fe529580cd0adb8
Gonzalo Martin <montalvog@gmail.com>
2025-12-31 21:45:44 -0300

    test - flujo de compra ok

--- src/pages/po_02_productos.py
+++ src/pages/po_02_productos.py
@@ -13,12 +13,14 @@ class PoProductos(PageFactory):
         # Productos
         "btn_home":            ("XPATH", "//header[@id='header']/div[@class='header-middle']/div[@class='container']/div[@class='row']/div[@class='col-sm-8']/div[@class='shop-menu pull-right']/ul[@class='nav navbar-nav']/li[1]/a"),
         "btn_productos":       ("XPATH", "//header[@id='header']/div[@class='header-middle']/div[@class='container']/div[@class='row']/div[@class='col-sm-8']/div[@class='shop-menu pull-right']/ul[@class='nav navbar-nav']/li[2]/a"),
+        "txt_buscar":          ("ID", "search_product"),
+        "btn_buscar":          ("XPATH", "//button[@id='submit_search']/i[@class='fa fa-search']"),
         "btn_kids":            ("XPATH", "//div[@id='accordian']/div[@class='panel panel-default'][3]/div[@class='panel-heading']/h4[@class='panel-title']/a[@class='collapsed']"),
         "btn_kids_sub_item":   ("XPATH", "//div[@id='Kids']/div[@class='panel-body']/ul/li[2]/a"),
         "lbl_kids_header":     ("XPATH", "/html/body/section/div[@class='container']/div[@class='row']/div[@class='col-sm-9 padding-right']/div[@class='features_items']/h2[@class='title text-center']"),
-        "btn_ver_producto":    ("XPATH", "/html/body/section/div[@class='container']/div[@class='row']/div[@class='col-sm-9 padding-right']/div[@class='features_items']/div[@class='col-sm-4'][1]/div[@class='product-image-wrapper']/div[@class='choose']/ul[@class='nav nav-pills nav-justified']/li/a"),
+        "btn_ver_producto":    ("XPATH", "/html/body/section[2]/div[@class='container']/div[@class='row']/div[@class='col-sm-9 padding-right']/div[@class='features_items']/div[@class='col-sm-4']/div[@class='product-image-wrapper']/div[@class='choose']/ul[@class='nav nav-pills nav-justified']/li/a"),
         "lbl_producto_sel":    ("XPATH", "/html/body/section/div[@class='container']/div[@class='row']/div[@class='col-sm-9 padding-right']/div[@class='product-details']/div[@class='col-sm-7']/div[@class='product-information']/h2"),
-        "btn_agregar_carrito": ("XPATH", "/html/body/section/div[@class='container']/div[@class='row']/div[@class='col-sm-9 padding-right']/div[@class='product-details']/div[@class='col-sm-7']/div[@class='product-information']/span/button[@class='btn btn-default cart']v"),
+        "btn_agregar_carrito": ("XPATH", "/html/body/section/div[@class='container']/div[@class='row']/div[@class='col-sm-9 padding-right']/div[@class='product-details']/div[@class='col-sm-7']/div[@class='product-information']/span/button[@class='btn btn-default cart']"),
         "lbl_agregado_ok":     ("XPATH", "//div[@id='cartModal']/div[@class='modal-dialog modal-confirm']/div[@class='modal-content']/div[@class='modal-header']/h4[@class='modal-title w-100']"),
         "btn_continuar":       ("XPATH", "//div[@id='cartModal']/div[@class='modal-dialog modal-confirm']/div[@class='modal-content']/div[@class='modal-footer']/button[@class='btn btn-success close-modal btn-block']"),
 
@@ -44,14 +46,13 @@ class PoProductos(PageFactory):
     
     def ingresar_productos(self):
         self.btn_productos.click()
-        self.btn_kids.element_to_be_clickable()
+        self.txt_buscar.element_to_be_clickable()
         me.get_screen_shot(self.name)
 
-    def filtrar_seleccion(self):
-        self.btn_kids.click()
-        self.btn_kids_sub_item.element_to_be_clickable()
+    def filtrar_seleccion(self, filtro):
+        self.txt_buscar.set_text(filtro)
         me.get_screen_shot(self.name)
-        self.btn_kids_sub_item.click()
+        self.btn_buscar.click()
         self.lbl_kids_header.element_to_be_clickable()
         me.get_screen_shot(self.name)
 
